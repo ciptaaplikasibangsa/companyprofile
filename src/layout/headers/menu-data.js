0989fea4ac6a5f1e5681d1c796c3096ff7daf93a
DevTeam MejaKita <mejakita.devteam@gmail.com>
2024-01-26 10:37:15 +0700

    project deploy

---
+++ src/layout/headers/menu-data.js
@@ -0,0 +1,24 @@
+const menu_data = [
+  {
+    id: 1,
+    mega_menu: false,
+    has_dropdown: false,
+    title: "Home",
+    link: "/"
+  },
+  {
+    id: 2,
+    mega_menu: false,
+    has_dropdown: false,
+    title: "About",
+    link: "/about",
+  },
+  {
+    id: 6,
+    mega_menu: false,
+    has_dropdown: false,
+    title: "Contact",
+    link: "/contact",
+  },
+];
+export default menu_data;
---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SEO from "../common/seo";
+import About from "../components/about";
+
+const index = () => {
+  return (
+    <>
+      <SEO pageTitle={"PT Cipta Aplikasi Bangsa - About"} />
+      <About />
+    </>
+  );
+};
+
+export default index;
